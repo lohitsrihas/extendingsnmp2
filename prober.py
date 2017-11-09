939baee21496b3e728174b02d2e165a95796bc56
dragstinger <31171347+lohitsrihas@users.noreply.github.com>
2017-11-09 20:04:10 +0100

    Updated according to the frequency and samples along with formatted output

--- prober.py
+++ prober.py
@@ -1,93 +1,107 @@
 import subprocess, sys
 import time
 
-oid = " ".join(sys.argv[3:])
-time_arg = sys.argv[2]
 delay1 = time.time()
-samp = float(time_arg)
-samp = int(samp)
+
+oid = " ".join(sys.argv[4:])
+freq_arg = sys.argv[2]
+time_samp = float(freq_arg)
+time_samp = 1/time_samp
+#time_samp = int(1/freq_samp)
+
+
+samples_arg = sys.argv[3]
+samples = float(samples_arg)
+samples = int(samples)
+
 args = sys.argv[1]
 args = args.split(':')
 ip = args[0]
 port = args[1]
 comm = args[2]
 
+if(samples >= 2):
+	loop = samples+1 
+elif(samples == -1):
+	loop = 1
+elif(samples == 0 or samples == 1):
+	loop = 0
+else:
+	print("Incorrect samples value entered")
+	exit()
+
+time_ticks11 = 0.0
+old_arr = []
+
+num = 1
 while 1:
-	
+
 	cmd = "snmpget -v1 -c"+comm+" "+ip+":"+port+" 1.3.6.1.2.1.1.3.0 "+oid
 
 	try:
 
-############### First/Old Counter Value Calculation Statements #######################	
+		############### Old Counter Value Calculation Statements #######################	
+		if(samples == -1):
+			pass
+		elif(num <= loop):
+			pass
+		else:
+			break
 
-		out1 = []
-		out1 = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
-		change_line1 = out1
-		change_line1 = change_line1.splitlines()
-		change_type1 = 0
-		time_ticks11 = 0
-		old_arr = []
-		uptime1 = change_line1[0]
-		uptime1 = uptime1.split(' ')
+		out = []
+		out = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
+		change_line = out
+		change_line = change_line.splitlines()
+		change_type = 0
+		time_ticks22 = 0.0
+		new_arr = []
+		uptime = change_line[0]
+		uptime = uptime.split(' ')
 		oid_id = []
-		time_ticks1 = uptime1[3]
-		time_ticks1 = time_ticks1.replace('(', '')
-		time_ticks1 = time_ticks1.replace(')', '')
-		time_ticks11 = float (time_ticks1)
+		time_ticks = uptime[3]
+		time_ticks = time_ticks.replace('(', '')
+		time_ticks = time_ticks.replace(')', '')
+		time_ticks22 = float (time_ticks)
 		i = 1
-		while i < len(change_line1):	
-			change_value1 = change_line1[i].split(' ')
-			oid_id.append(change_value1[0])
-			change_type1 = float (change_value1[3])	
-			old_arr.append(change_type1)
+		while i < len(change_line):	
+			change_value = change_line[i].split(' ')
+			oid_id.append(change_value[0])
+			change_type = float (change_value[3])	
+			new_arr.append(change_type)
 			i += 1
 		delay2 = time.time()
-		samp_new = samp - (delay2 - delay1)
+		samp_new = time_samp - (delay2 - delay1)
 		if(samp_new < 0):
 			pass
 		else:
 			time.sleep(samp_new)
 		delay1 = time.time()
-	
-###############	Second/New Counter Value Calculation Statements #######################
-		out2 = []
-		out2 = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
-		change_line2 = out2
-		change_line2 = change_line2.splitlines()
-		change_type2 = 0
-		time_ticks22 = 0
-		uptime2 = change_line2[0]
-		uptime2 = uptime2.split(' ')
-		time_ticks2 = uptime2[3]
-		time_ticks2 = time_ticks2.replace('(', '')
-		time_ticks2 = time_ticks2.replace(')', '')
-		time_ticks22 = float (time_ticks2)
-		new_arr = []
-		j = 1
-		while j < len(change_line2):	
-			change_value2 = change_line2[j].split(' ')		
-			change_type2 = float (change_value2[3])		
-			new_arr.append(change_type2)
-			j += 1
-	
 		
-############## Rate of Change of Counter Values Calculation Statements #################
-		time_ticks = (time_ticks22 - time_ticks11)/100
-		if(time_ticks < 0):
-			print ("The Device has been Rebooted")
+		############## Rate of Change of Counter Values Calculation Statements #################
+		output = []
+		if(num == 1):
+			pass
 		else:
-			k = 0
-			while k < len(new_arr):
-				rate_calc = new_arr[k]-old_arr[k]
-				if(rate_calc < 0):
-					rate_calc = rate_calc + 0xffffffff
-					rate_calc = rate_calc/time_ticks
-					print oid_id[k], " Rate : ", int(round(rate_calc))
-				else:
-					rate_calc = rate_calc/time_ticks
-					print oid_id[k], " Rate : ", int(round(rate_calc))
-				k += 1
-		print "########################################################"
-	except subprocess.CalledProcessError as e:
+			time_ticks = (time_ticks22 - time_ticks11)/100
+			if(time_ticks < 0):
+				print ("The Device has been Rebooted")
+			else:
+				output.append(int(round(time.time())))
+				k = 0
+				while k < len(new_arr):	
+					rate_calc = new_arr[k]-old_arr[k]
+					if(rate_calc < 0):
+						rate_calc = rate_calc + 0xffffffff
+						rate_calc = rate_calc/time_ticks
+						output.append(int(round(rate_calc)))
+					else:	
+						rate_calc = rate_calc/time_ticks
+						output.append(int(round(rate_calc)))
+					k = k + 1
+		print ' | '.join(map(str, output))
+		old_arr = new_arr
+#		print "########################################################"
+		time_ticks11 = time_ticks22		
+	except subprocess.CalledProcellssError as e:
 		print e.output
-	
+	num = num + 1
